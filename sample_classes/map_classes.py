b287f27538e7ded19c82d24f7f0643890bc23864
Andrei-Raev <andrey-raev.raev@yandex.ru>
2020-12-15 20:39:54 +0300

    world_class v 0.3

---
+++ sample_classes/map_classes.py
@@ -0,0 +1,48 @@
+class obekt:
+    def __init__(self, y, x, preor=3):
+        self.y = y
+        self.x = x
+        self.preor = preor
+        
+    def get_x(self):
+        return self.x
+    
+    def get_y(self):
+        return self.y
+    
+    def get_preor(self):
+        return self.preor
+    
+    def get_pos(self):
+        return (self.y, self.x)    
+
+
+class relef(obekt):
+    def __init__(self, y, x, preor=3):
+        self.y = y
+        self.x = x
+        self.preor = preor
+
+    
+    
+class poctr(obekt):
+    def __init__(self, y, x, preor=2):
+        self.y = y
+        self.x = x
+        self.preor = preor
+        
+    
+class entiti(obekt):
+    def __init__(self, y, x, hp, preor=1):
+        self.y = y
+        self.x = x
+        self.preor = preor
+        self.hp = hp
+        
+    def get_hp(self):
+        return self.hp
+    
+    
+class predmet():
+    def __init__(self):
+        pass
