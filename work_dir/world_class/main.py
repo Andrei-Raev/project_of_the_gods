101ff9fb35a38abd32bfba1b012df42fb86a7e02
Andrei-Raev <andrey-raev.raev@yandex.ru>
2020-12-15 19:24:04 +0300

    world_class v 0.2

--- work_dir/world_class/main.py
+++ work_dir/world_class/main.py
@@ -2,13 +2,15 @@ import random
 import time
 from copy import copy
 from screeninfo import get_monitors
+from numba import cuda
+from threading import currentThread
 
 import pygame
 from math import sin, cos
 
 pygame.init()
 
-fullscreen = True
+fullscreen = False
 
 if fullscreen:
     size = width, height = get_monitors()[0].width, get_monitors()[0].height
@@ -52,6 +54,25 @@ class World:  # Класс мира
         pass
 
 
+
+def global_render_chunk(board):
+    ground = pygame.Surface((1020, 1020))
+    for i in board['landscape']:
+        t = i.get_type()
+        if t == 1:
+            block = pygame.image.load('grass.png').convert()
+        elif t == 2:
+            block = pygame.image.load('stone.png').convert()
+        else:
+            block = pygame.image.load('sand.png').convert()
+        cord = i.get_cord()
+        block_rect = block.get_rect(topleft=(tuple([j * 32 for j in cord])))
+        ground.blit(block, block_rect)
+
+
+    return ground
+
+
 class Chunk:  # Класс чанка мира
     def __init__(self, seed: int, cord: (int, int)):
         self.seed = seed
@@ -60,26 +81,26 @@ class Chunk:  # Класс чанка мира
         self.board = {'landscape': set(), 'buildings': {}, 'mechanisms': {}, 'entities': {}}
         self.ground = pygame.Surface((1020, 1020))
 
+        self.blocks = [pygame.image.load('grass.png').convert(), pygame.image.load('stone.png').convert(), pygame.image.load('sand.png').convert()]
+
     def generate_сhunk(self) -> None:
         random.seed(self.seed)
+        del self.board
+        self.board = {'landscape': set(), 'buildings': {}, 'mechanisms': {}, 'entities': {}}
         for y in range(32):
             for x in range(32):
                 self.board['landscape'].add(Block((x, y), random.randint(0, 2)))
 
+
     def render_chunk(self) -> None:
+        del self.ground
         self.ground = pygame.Surface((1020, 1020))
-        # self.ground.fill(55, 5, 4)
+        self.ground.fill((55, 5, 4))
         for i in self.board['landscape']:
-            t = i.get_type()
-            if t == 1:
-                block = pygame.image.load('grass.png').convert()
-            elif t == 2:
-                block = pygame.image.load('stone.png', '').convert()
-            else:
-                block = pygame.image.load('sand.png').convert()
             cord = i.get_cord()
-            block_rect = block.get_rect(topleft=(tuple([j * 32 for j in cord])))
-            self.ground.blit(block, block_rect)
+            block_rect = self.blocks[i.get_type()].get_rect(topleft=(tuple([j * 32 for j in cord])))
+            self.ground.blit(self.blocks[i.get_type()], block_rect)
+            del cord, i, block_rect,
 
     def get_s(self):
         return self.ground
@@ -90,6 +111,16 @@ map_c = [0, 0]
 tmp = Chunk(1, (0, 0))
 tmp.generate_сhunk()
 tmp.render_chunk()
+
+start_time_m = time.time()
+for _ in range(10):
+    start_time = time.time()
+    tmp.seed = tmp.seed + 1
+    tmp.generate_сhunk()
+    tmp.render_chunk()
+    print("--- %s seconds ---" % (time.time() - start_time))
+print()
+print("--- %s seconds --- MAIN" % (time.time() - start_time_m))
 if __name__ == '__main__':
     running = True
 
