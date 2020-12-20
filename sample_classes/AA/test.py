734ed8e9db6f33c011658f827807aa7ec0945423
Andrei-Raev <andrey-raev.raev@yandex.ru>
2020-12-20 22:45:05 +0300

    AA v 0.3

--- sample_classes/AA/test.py
+++ sample_classes/AA/test.py
@@ -1,8 +1,50 @@
 import random
+import time
+from threading import Thread
 
 import pygame
 
 from PIL import Image
+
+pygame.init()
+
+def init_screen_and_clock():
+    global screen, display, clock
+    pygame.init()
+    WINDOW_SIZE = (1150, 640)
+    pygame.display.set_caption('Game')
+    screen = pygame.display.set_mode(WINDOW_SIZE, 0, 32)
+    clock = pygame.time.Clock()
+
+
+def create_fonts(font_sizes_list):
+    "Creates different fonts with one list"
+    fonts = []
+    for size in font_sizes_list:
+        fonts.append(
+            pygame.font.SysFont("Arial", size))
+    return fonts
+
+
+def render(fnt, what, color, where):
+    "Renders the fonts as passed from display_fps"
+    text_to_show = fnt.render(what, 0, pygame.Color(color))
+    screen.blit(text_to_show, where)
+
+
+def display_fps():
+    "Data that will be rendered and blitted in _display"
+    render(
+        fonts[0],
+        what=str(int(clock.get_fps())),
+        color="white",
+        where=(0, 0))
+
+
+init_screen_and_clock()
+fonts = create_fonts([32, 16, 14, 8])
+
+
 
 
 def antialiasing(surface, size_cof):
@@ -12,13 +54,12 @@ def antialiasing(surface, size_cof):
 
     raw_str = pygame.image.tostring(surface, strFormat, False)
     image = Image.frombytes(strFormat, surface.get_size(), raw_str)
-    # image.show()
+
     image = image.resize(size, resample=Image.ANTIALIAS)
 
     raw_str = image.tobytes("raw", strFormat)
-    # image.show()
     result = pygame.image.fromstring(raw_str, image.size, strFormat)
-
+    del size, strFormat, raw_str, image
     return result
 
 
@@ -28,20 +69,31 @@ WIDTH = 720
 size = width, height = 700, 700
 
 if __name__ == '__main__':
-    pygame.init()
+    fps = 60
     screen = pygame.display.set_mode(size)
     pygame.display.set_caption('None')
-    screen.fill((0, 0, 0))
 
-    COF = 4
-    tmp = pygame.Surface((600 * COF, 600 * COF))
 
-    pygame.draw.circle(tmp, (255, 255, 255), (300 * COF, 300 * COF), 300 * COF, 0)
-    tmp = antialiasing(tmp, COF)
+    COF = 2
+    xx, yy = (300, 300)
+    running = True
+    while running:
+        screen.fill((0, 0, 0))
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+            elif event.type == pygame.MOUSEMOTION:
+                xx += event.rel[0]
+                yy += event.rel[1]
 
-    # ...
-    screen.blit(tmp, (50, 50))
-    pygame.display.flip()
-    while pygame.event.wait().type != pygame.QUIT:
-        pass
+        start_time = time.time()
+        tmp = pygame.Surface((600 * COF, 600 * COF))
+        pygame.draw.circle(tmp, (255, 255, 255), (xx * COF, yy * COF), 300 * COF, 0)
+        tmp = antialiasing(tmp, COF)
+
+        screen.blit(tmp, (50, 50))
+        display_fps()
+        pygame.display.flip()
+        print("--- %s seconds ---" % (time.time() - start_time))
+        clock.tick(fps)
     pygame.quit()
