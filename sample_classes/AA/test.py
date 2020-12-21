53a4f6876560faa4f5227a4ce1eb6669ac4d951f
Andrei-Raev <andrey-raev.raev@yandex.ru>
2020-12-21 15:37:32 +0300

    BLUR v 0.1

--- sample_classes/AA/test.py
+++ sample_classes/AA/test.py
@@ -4,9 +4,10 @@ from threading import Thread
 
 import pygame
 
-from PIL import Image
+from PIL import Image, ImageFilter
 
 pygame.init()
+
 
 def init_screen_and_clock():
     global screen, display, clock
@@ -45,8 +46,6 @@ init_screen_and_clock()
 fonts = create_fonts([32, 16, 14, 8])
 
 
-
-
 def antialiasing(surface, size_cof):
     size = surface.get_size()
     size = [i // size_cof for i in size]
@@ -56,6 +55,21 @@ def antialiasing(surface, size_cof):
     image = Image.frombytes(strFormat, surface.get_size(), raw_str)
 
     image = image.resize(size, resample=Image.ANTIALIAS)
+
+    raw_str = image.tobytes("raw", strFormat)
+    result = pygame.image.fromstring(raw_str, image.size, strFormat)
+    del size, strFormat, raw_str, image
+    return result
+
+
+def blur(surface, rad):
+    size = surface.get_size()
+    strFormat = 'RGBA'
+
+    raw_str = pygame.image.tostring(surface, strFormat, False)
+    image = Image.frombytes(strFormat, surface.get_size(), raw_str)
+
+    image = image.filter(ImageFilter.BoxBlur(rad))
 
     raw_str = image.tobytes("raw", strFormat)
     result = pygame.image.fromstring(raw_str, image.size, strFormat)
@@ -73,8 +87,7 @@ if __name__ == '__main__':
     screen = pygame.display.set_mode(size)
     pygame.display.set_caption('None')
 
-
-    COF = 2
+    COF = 1
     xx, yy = (300, 300)
     running = True
     while running:
@@ -83,13 +96,18 @@ if __name__ == '__main__':
             if event.type == pygame.QUIT:
                 running = False
             elif event.type == pygame.MOUSEMOTION:
-                xx += event.rel[0]
-                yy += event.rel[1]
+                # xx += event.rel[0]
+                # yy += event.rel[1]
+                pass
 
         start_time = time.time()
         tmp = pygame.Surface((600 * COF, 600 * COF))
-        pygame.draw.circle(tmp, (255, 255, 255), (xx * COF, yy * COF), 300 * COF, 0)
-        tmp = antialiasing(tmp, COF)
+        pygame.draw.circle(tmp, (255, 255, 255), (xx * COF, yy * COF), 270 * COF, 0)
+        #f = pygame.font.Font(None, 150)
+        #tt = f.render('fefef', False, (0,255, 0))
+        #ss = tmp.get_size()
+        #tmp.blit(tt, ([i // 2 for i in ss]))
+        tmp = blur(tmp, 25)
 
         screen.blit(tmp, (50, 50))
         display_fps()
