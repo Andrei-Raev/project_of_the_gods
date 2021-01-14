49b7e9c13d1d84e387c31e40fa842a60befb6400
Andrei-Raev <andrey-raev.raev@yandex.ru>
2021-01-14 18:37:32 +0300

    loading.py v 1.0 work... Work. WORK!!!

--- sample_classes/animations/loading.py
+++ sample_classes/animations/loading.py
@@ -14,49 +14,69 @@ screen.fill(BLACK)
 
 
 def draw_arrow(surf, color, start_cords, end_cords, width):
-    rad_ar = width * 3
-    pygame.draw.line(surf, color,
-                     start_cords,
-                     [tt - .5 * width for tt in end_cords], width)
+    rad_ar = width * 1.5
 
     points = []
 
     tmp_x = start_cords[0] - end_cords[0]
     tmp_y = start_cords[1] - end_cords[1]
+
     try:
         rotate = tmp_x / tmp_y
     except ZeroDivisionError:
         rotate = 0
-        print(0)
+
     rotate = degrees(atan(rotate))
 
     for iters in range(3):
-        if start_cords[1] - end_cords[1] > 0:
-            tmp = radians(120 * iters + rotate/2)
+
+        if tmp_x * tmp_y < 0:
+            tmp = radians(120 * iters - rotate + 30)
         else:
-            tmp = radians(120 * iters - rotate/2)
-        points.append((sin(tmp) * rad_ar + end_cords[0], cos(tmp) * rad_ar + end_cords[1]))
+            tmp = radians(120 * iters - rotate - 30)
 
-    pygame.draw.polygon(surf, color, points)
+        if tmp_x > 0:
+            tmp -= 1
 
+        points.append((cos(tmp) * rad_ar + end_cords[0], sin(tmp) * rad_ar + end_cords[1]))
+
+    main_points = []
+
+    tmp = radians(-rotate)
+    main_points.append((cos(tmp) * width / 2 + start_cords[0], sin(tmp) * width / 2 + start_cords[1]))
+
+    tmp = radians(180 - rotate)
+    main_points.append((cos(tmp) * width / 2 + start_cords[0], sin(tmp) * width / 2 + start_cords[1]))
+
+    main_points.append((cos(tmp) * width / 2 + end_cords[0], sin(tmp) * width / 2 + end_cords[1]))
+
+    tmp = radians(-rotate)
+    main_points.append((cos(tmp) * width / 2 + end_cords[0], sin(tmp) * width / 2 + end_cords[1]))
+
+    pygame.draw.polygon(surf, color, main_points, 0)
+    pygame.draw.polygon(surf, color, points, 0)
 
 
 def draw_circle_lines(surf, rads, rotate=False):
     for iters in range(3):
         if rotate:
             tmp = radians(120 * iters)
+            draw_arrow(surf, WHITE,
+                       (sin(tmp) * rads[0] + 300, cos(tmp) * rads[0] + 300),
+                       (sin(tmp) * rads[1] + 300, cos(tmp) * rads[1] + 300), 10)
         else:
             tmp = radians(120 * iters + 60)
-        draw_arrow(surf, WHITE,
-                   (sin(tmp) * rads[0] + 300, cos(tmp) * rads[0] + 300),
-                   (sin(tmp) * rads[1] + 300, cos(tmp) * rads[1] + 300), 5)
+            draw_arrow(surf, WHITE,
+                       (sin(tmp) * rads[1] + 300, cos(tmp) * rads[1] + 300),
+                       (sin(tmp) * rads[0] + 300, cos(tmp) * rads[0] + 300), 10)
 
 
-draw_circle_lines(screen, [50, 100])
-draw_circle_lines(screen, [100, 200], True)
-pygame.draw.circle(screen, WHITE, (300, 300), 100, 1)
+draw_circle_lines(screen, [30, 98])
+draw_circle_lines(screen, [100, 180], True)
+pygame.draw.circle(screen, WHITE, (300, 300), 100, 10)
 
-draw_arrow(screen,WHITE, (100, 230), (100, 300), 10)
+clock = pygame.time.Clock()
+clock.tick(60)
 
 pygame.display.flip()
 while pygame.event.wait().type != pygame.QUIT:
