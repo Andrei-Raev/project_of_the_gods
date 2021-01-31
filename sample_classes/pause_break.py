a6f72570e53f940763fa1c525206933cdafce987
Andrei-Raev <andrey-raev.raev@yandex.ru>
2021-01-31 19:48:05 +0300

    Anim?

--- sample_classes/pause_break.py
+++ sample_classes/pause_break.py
@@ -217,6 +217,7 @@ MAP_COF = 1
 WORLD_SIZE = {'small': 100, 'medium': 250, 'large': 500}
 WORLD_NOISE_SIZE = 50
 BLOCK_SIZE = MAP_COF * 32
+FONT_PATH = r'D:\temp\project_of_the_gods\work_dir\main_menu\font.ttf'
 
 # ---------- VARIABLES ----------
 fonts = create_fonts([32, 16, 14, 8])
@@ -237,26 +238,120 @@ else:
 pygame.display.set_caption('Pause break')
 
 
+class MenuButton:
+    def __init__(self, x: int, y: int, button_size: tuple, border: int, text: str, text_size: int,
+                 surf: pygame.surface.Surface):
+        self.color = (0, 0, 34)
+        self.surf = surf
+        self.font = pygame.font.Font(FONT_PATH, text_size)
+
+        self.border_color = (0, 22, 87)
+        self.x = x
+        self.y = y
+        self.button_size = button_size
+        self.text = str(text)
+        self.text_color = (243, 246, 250)
+        tmp = self.font.render(self.text, True, self.text_color)
+        self.border_width = border
+        self.cords = (0, 0)
+
+        self.anim_hover = 0
+
+        self.on_click_set = [[False], [False], [False]]
+        self.width, self.height = self.button_size
+
+        self.font_render = self.font.render(self.text, True, self.text_color)
+
+        self.button_surf = pygame.surface.Surface((self.button_size[0], self.button_size[1]))
+        self.button_surf.set_colorkey((0, 0, 0))
+        self.button_surf.convert_alpha()
+        pygame.draw.rect(self.button_surf, self.border_color, (0, 0, *self.button_size), 0, 20)
+
+        pygame.draw.rect(self.button_surf, self.color, (
+            self.border_width, self.border_width, self.button_size[0] - self.border_width * 2,
+            self.button_size[1] - self.border_width * 2), 0, 30)
+        self.button_surf.blit(self.font_render, ((self.width - self.font_render.get_width()) // 2,
+                                                 (self.height - self.font_render.get_height()) // 2))
+
+    def draw(self, event):
+        rect = self.button_surf.get_rect(topleft=(self.x - self.anim_hover, self.y))
+        if rect.collidepoint(pygame.mouse.get_pos()):
+
+            if event:
+                event = event[0]
+                if event.button == 1 and self.on_click_set[0][0]:
+                    self.on_click_set[0][1](*self.on_click_set[0][2])
+                elif event.button == 2 and self.on_click_set[1][0]:
+                    self.on_click_set[1][1](*self.on_click_set[1][2])
+                elif event.button == 3 and self.on_click_set[2][0]:
+                    self.on_click_set[2][1](*self.on_click_set[2][2])
+
+            self.anim_hover += 2
+            if self.anim_hover >= 50:
+                self.anim_hover = 50
+        else:
+            self.anim_hover -= 1
+            if self.anim_hover <= 1:
+                self.anim_hover = 1
+
+        self.surf.blit(self.button_surf, (self.x - self.anim_hover, self.y))
+
+    def set_on_click(self, func, but: int, *args):
+        self.on_click_set[but - 1] = [True, func, args]
+
+
 class PauseScreen:
     def __init__(self, size, background):
-        self.font = pygame.font.Font(r'D:\temp\project_of_the_gods\work_dir\main_menu\font.ttf', 50)
+        self.font = pygame.font.Font(FONT_PATH, 60)
         self.screen = pygame.surface.Surface(size)
         self.background = background
-        self.margin = self.screen.get_size()[0] * 0.25
+        self.margin = self.screen.get_size()[0] * 0.4
 
-    def render(self, surf: pygame.surface.Surface):
+        self.pause_text = pygame.font.Font(FONT_PATH, 200).render('Пауза', True, (243, 246, 250))
+
+        self.buttons = [
+            MenuButton(width - width // 4, height - (height // 5.5) * 3, (width * .2, height * .15), width // 80,
+                       "Продолжить", width // 22, self.screen),
+            MenuButton(width - width // 4, height - (height // 5.5) * 2, (width * .2, height * .15), width // 80,
+                       "Настройки", width // 22, self.screen),
+            MenuButton(width - width // 4, height - (height // 5.5) * 1, (width * .2, height * .15), width // 80,
+                       "Выход", width // 22, self.screen)]
+
+        self.buttons[2].set_on_click(lambda: exit(), 1)
+
+        self.date_update('')
+
+    def render(self, surf: pygame.surface.Surface, events):
+
         self.screen = pygame.surface.Surface(size)
         self.screen.set_colorkey((0, 0, 0))
         self.screen = self.screen.convert_alpha()
 
-        pygame.draw.rect(self)
+        pygame.draw.rect(self.screen, (0, 0, 22), (0, 0, width * .4, height))
         for i in range(510):
             pygame.draw.line(self.screen,
-                             (0, 0, 0, abs((i - 500) // 2)),
+                             (0, 0, 22, abs((i - 500) // 2)),
                              (i + self.margin, 0), (i + self.margin, self.screen.get_size()[1]))
+
+        self.screen.blit(self.pause_text, (width * .05, height * .03))
+        self.screen.blit(self.time_text, (width * .05, height * .35))
+
+        for text in enumerate(self.info_text):
+            self.screen.blit(text[1], (width * .05, height * (.45 + 0.07 * text[0])))
+
+        for bt in self.buttons:
+            bt.surf = self.screen
+            bt.draw(list(filter(lambda xx: xx.type == pygame.MOUSEBUTTONUP, events)))
 
         surf.blit(self.background, (0, 0))
         surf.blit(self.screen, (0, 0))
+
+    def date_update(self, data: str):
+        self.time_text = self.font.render('', True, (243, 246, 250))  # Текущее время: 16-35', True, (243, 246, 250))
+        self.info_text = []
+
+        for text in data.split('\n'):
+            self.info_text.append(self.font.render(text, True, (243, 246, 250)))
 
 
 def pause():
@@ -270,7 +365,8 @@ def pause():
     pause_obj = PauseScreen(size, background)
 
     while is_open:
-        for event in pygame.event.get():
+        events = pygame.event.get()
+        for event in events:
             if event.type == pygame.QUIT:
                 main_running = False
                 return
@@ -288,7 +384,7 @@ def pause():
 
         # screen.blit(background, (0, 0))
 
-        pause_obj.render(screen)
+        pause_obj.render(screen, events)
 
         display_fps()
         clock.tick(fps)
@@ -297,7 +393,7 @@ def pause():
 
 if __name__ == '__main__':
     main_running = True
-    im = pygame.transform.scale(pygame.image.load('im.png'), (int(1280 / 1), int(1231 / 1)))
+    im = pygame.transform.scale(pygame.image.load('im.png').convert(), (int(1280 / 1), int(1231 / 1)))
 
     while main_running:
         ev = pygame.event.get()
